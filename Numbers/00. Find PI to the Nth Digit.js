6111e49eb979b97b29be831f231f522f1a85397a
Potecaru Tudor <tudor.potecaru@gmail.com>
2013-11-01 15:58:11 +0200

    Fibonacci Sequence

--- Numbers/00. Find PI to the Nth Digit.js
+++ Numbers/00. Find PI to the Nth Digit.js
@@ -1,9 +1,14 @@
 /*
-	Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
-	Based on John Machin's formula pi/4 = 4 * arctan (1/5) - arctan (1/239)
+	Numbers 00. Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
+				Based on John Machin's formula pi/4 = 4 * arctan (1/5) - arctan (1/239)
 */
 
-var calculatePI = function(n){
+var getPI = function (n) {
 	"use strict";
+
+	if (n === undefined || n > 20) {
+		n = 20;
+	}
+
 	return (16 * Math.atan(1 / 5) - 4 * Math.atan(1 / 239)).toFixed(n);
 };
