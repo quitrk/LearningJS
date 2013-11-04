8a412961fef695cbd0e5e2b8d1dfde9e3745184a
Potecaru Tudor <tudor.potecaru@gmail.com>
2013-11-04 15:09:32 +0200

    Collatz Conjecture

---
+++ Classic Algorithms/Collatz Conjecture.js
@@ -0,0 +1,29 @@
+/*
+    Classic Algorithms 00. Collatz Conjecture - Start with a number n > 1. Find the number of steps it takes to reach one using the following process: If n is even, divide it by 2. 
+    If n is odd, multiply it by 3 and add 1.
+*/
+
+var collatz = function (n) {
+    "use strict";
+
+    var steps = 0;
+
+    (function recursive(n) {
+        if (n <= 1) {
+            return;
+        }
+
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = n * 3 + 1;
+        }
+
+        steps++;
+        recursive(n);
+    }(n));
+
+    return steps;
+};
+
+this.console.log(collatz(20));
