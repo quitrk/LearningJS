bdce186c7d452bd54a0da108dd357a3d9dd06ddf
Chris Shimmin <cgshim11@yahoo.com>
2016-12-14 14:33:47 -0500

    Update 00. Find PI to the Nth Digit.js

--- Numbers/00. Find PI to the Nth Digit.js
+++ Numbers/00. Find PI to the Nth Digit.js
@@ -2,15 +2,18 @@
     Numbers 00. Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
                 Based on John Machin's formula pi/4 = 4 * arctan (1/5) - arctan (1/239)
 */
+var numPi = document.getElementById("numPi");
 
-var getPI = function (n) {
-    "use strict";
-
-    if (n === undefined || n > 20) {
-        n = 20;
+function piDigits() {
+    var digits = parseFloat(numPi.value);
+    
+    if ( digits > 20 || digits === NaN ) {
+        digits = 20;
     }
+        var answer = Math.PI.toFixed(digits);
 
-    return (16 * Math.atan(1 / 5) - 4 * Math.atan(1 / 239)).toFixed(n);
+    pi.innerHTML = answer;
 };
 
-this.console.log(getPI(20));
+numPi.addEventListener("input", piDigits);
+console.log(Math.PI.toFixed(2)); //display console too
